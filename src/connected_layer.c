74dd819ba1d345aa92096a38b02264da1b0d88d1
hide27k <hide27k@cs.washington.edu>
2020-10-13 23:03:52 -0700

    Fix conflict.

--- src/connected_layer.c
+++ src/connected_layer.c
@@ -66,19 +66,11 @@ void backward_connected_layer(layer l, matrix prev_delta)
     gradient_matrix(out, l.activation, delta); 
     // Calculate the updates for the bias terms using backward_bias
     // The current bias deltas are stored in l.db
-<<<<<<< HEAD
-    backward_bias(delta, l.db);
-    // Then calculate dL/dw. Use axpy to add this dL/dw into any previously stored
-    // updates for our weights, which are stored in l.dw
-    matrix delta_w = matmul(transpose_matrix(in), delta);
-    axpy_matrix(1, delta_w, l.dw);
-=======
 
     // Then calculate dL/dw. Use axpy to subtract this dL/dw into any previously stored
     // updates for our weights, which are stored in l.dw
     // l.dw = l.dw - dL/dw
 
->>>>>>> 0dd28f5de96ccfb58a13812d0ad69044084540ac
     if(prev_delta.data){
         // Finally, if there is a previous layer to calculate for,
         // calculate dL/d(in). Again, using axpy, add this into the current
@@ -91,14 +83,6 @@ void backward_connected_layer(layer l, matrix prev_delta)
 // Update 
 void update_connected_layer(layer l, float rate, float momentum, float decay)
 {
-<<<<<<< HEAD
-    axpy_matrix(-decay, l.w,l.dw);
-    axpy_matrix(rate, l.dw, l.w);
-    scal_matrix(momentum, l.dw);
-    axpy_matrix(-decay, l.b,l.db);
-    axpy_matrix(rate, l.db, l.b);
-    scal_matrix(momentum, l.db);
-=======
     // TODO: 3.3
     // Currently l.dw and l.db store:
     // l.dw = momentum * l.dw_prev - dL/dw
@@ -116,7 +100,6 @@ void update_connected_layer(layer l, float rate, float momentum, float decay)
     // l.dw *= momentum
     // l.db *= momentum
 
->>>>>>> 0dd28f5de96ccfb58a13812d0ad69044084540ac
 }
 
 layer make_connected_layer(int inputs, int outputs, ACTIVATION activation)
